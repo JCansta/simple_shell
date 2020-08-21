98493802c9e4bf0fc6a4355dca290002b186c069
mauroxcf <maurox03@gmail.com>
2020-08-21 01:49:42 +0000

    adding strcmp

--- strfunctions.c
+++ strfunctions.c
@@ -4,7 +4,7 @@
  * _strncmp - string compare
  * @s1: String compare 1
  * @s2: String compare 2
- *
+ * @n: bites
  * Return: 0 or diference.
  */
 
@@ -29,7 +29,7 @@ int _strncmp(char *s1, char *s2, size_t n)
  * _strncmp - string compare
  * @s1: String compare 1
  * @s2: String compare 2
- *
+ * @n: bites
  * Return: 0 or diference.
  */
 
@@ -39,6 +39,31 @@ int _strncmp(char *s1, char *s2, size_t n)
     size_t counter;
 
     for (counter = 0; counter < n && s1[counter] != '\0'; counter++)
+    {
+        if (s1[counter] != s2[counter])
+        {
+            return (s1[counter] - s2[counter]);
+        }
+
+    }
+
+    return (0);
+}
+
+/**
+ * _strcmp - string compare
+ * @s1: String compare 1
+ * @s2: String compare 2
+ *
+ * Return: 0 or diference.
+ */
+
+int _strcmp(char *s1, char *s2)
+{
+
+    int counter;
+
+    for (counter = 0; s1[counter] != '\0'; counter++)
     {
         if (s1[counter] != s2[counter])
         {
