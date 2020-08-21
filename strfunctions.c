8056bd871544ff86f1f6844e2974dbf0252e5f8d
mauroxcf <maurox03@gmail.com>
2020-08-21 01:52:06 +0000

    adding strlen

--- strfunctions.c
+++ strfunctions.c
@@ -97,3 +97,21 @@ char *_strcpy(char *dest, char *src)
 
     return (dest);
 }
+
+/**
+ * _strlen - calculates the length of the string.
+ * @s:pointer variable.
+ * Return: Always 0.
+ */
+
+int _strlen(char *s)
+{
+    int a;
+
+    for (a = 0; s[a] != '\0'; a++)
+    {
+    }
+    return (a);
+
+
+}
